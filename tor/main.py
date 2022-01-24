82de43abcd30c102d60e56fcc681699c6a1f6061
ttschnz <tim@mw3.ch>
2022-01-24 08:45:24 +0100

    wait for hostname

--- tor/main.py
+++ tor/main.py
@@ -1,19 +1,30 @@
 import os, time
 print("waiting for web to be reachable")
+# wait for server to be up
 while True:
-    time.sleep(5)
     returnState = os.system("ping -c 1 web")
     print("returnState:",returnState)
     if returnState == 0:
         print("web is up")
         break
     print("web is not yet up")
+    time.sleep(5)
+
+# start service
 print("attempting to start tor")
 print(os.popen("service tor start").read())
-with open("/var/lib/tor/service/hostname") as f:
-    print("hostname:", f.read())
+
+# wait for hostname
 while True:
+    if os.path.exists("/var/lib/tor/service/hostname"):
+        with open("/var/lib/tor/service/hostname") as f:
+            print("hostname:", f.read())
+        break
     time.sleep(5)
+
+# exit if service stops
+while True:
     if os.popen("service tor status").read().find("running") == -1:
         exit(1)
         break
+    time.sleep(5)
