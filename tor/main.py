b00682176354daa7b91751ec37618ea305efbe8f
ttschnz <tim@mw3.ch>
2022-01-24 08:26:26 +0100

    debugging mode on

--- tor/main.py
+++ tor/main.py
@@ -2,7 +2,9 @@ import os, time
 print("waiting for web to be reachable")
 while True:
     time.sleep(5)
-    if not os.system("ping -c 1 web") == 0:
+    returnState = os.system("ping -c 1 web")
+    print("returnState:",returnState)
+    if not returnState == 0:
         print("web is up")
         break
     print("web is not yet up")
